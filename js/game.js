5235d5462d90fbd2335219e8b132d16f51027273
mellaniesoriano <23709723+mellaniesoriano@users.noreply.github.com>
2018-01-25 19:00:35 -1000

    phaser setup, initial commit

--- js/game.js
+++ js/game.js
@@ -1,3 +1,31 @@
 (Phaser => {
-  console.log(Phaser);
+  const GAME_WIDTH = 460;
+  const GAME_HEIGHT = 600;
+  const GAME_CONTAINER_ID = 'game';
+  const GFX = 'gfx';
+
+  const game = new Phaser.Game(
+    GAME_WIDTH,
+    GAME_HEIGHT,
+    Phaser.AUTO,
+    GAME_CONTAINER_ID, // binds  to html element (DOM)
+    { preload, create, update }
+  );
+
+  let player;
+
+  function preload() {
+    game.load.spritesheet(
+      GFX,
+      '../assets/shmup-spritesheet-140x56-28x28-tile.png',
+      28,
+      28
+    );
+  }
+
+  function create() {
+    game.add.sprite(100, 100, GFX, 8);
+  }
+
+  function update() {}
 })(window.Phaser);
